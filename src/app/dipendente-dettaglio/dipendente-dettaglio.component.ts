3cea2098dfb0e56a4fc9d239a23e6fdaf128b27b
MAugelli <m.augelli2@gmail.com>
2023-01-19 16:18:49 +0100

    add input

--- src/app/dipendente-dettaglio/dipendente-dettaglio.component.ts
+++ src/app/dipendente-dettaglio/dipendente-dettaglio.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Dipendente } from '../dipendente';
 
 @Component({
   selector: 'app-dipendente-dettaglio',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./dipendente-dettaglio.component.css']
 })
 export class DipendenteDettaglioComponent {
+
+  @Input() data:Dipendente[]=[]
+
+  
 
 }
